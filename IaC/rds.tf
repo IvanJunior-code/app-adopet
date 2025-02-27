aa35d06a4c6c5a209266a3441c60629ab758739e
Ivan Junior <ivan_junior_we@hotmail.com>
2025-02-26 22:36:21 -0300

    Adicionando db_subnet_group e vpc_security_group, e corrigindo parameter_group_name de 15 para 16

--- IaC/rds.tf
+++ IaC/rds.tf
@@ -1,12 +1,14 @@
 resource "aws_db_instance" "rds_postgres" {
-  allocated_storage    = 10
-  db_name              = "postgres"
-  engine               = "postgres"
-  port                 = 5432
-  engine_version       = "16.5" # aws rds describe-db-engine-versions --default-only --engine postgres
-  instance_class       = "db.t3.micro"
-  username             = "postgres"
-  password             = var.postgres_password
-  parameter_group_name = "default.postgres15"
-  skip_final_snapshot  = true
+  allocated_storage      = 10
+  db_name                = "postgres"
+  engine                 = "postgres"
+  port                   = 5432
+  engine_version         = "16.5" # aws rds describe-db-engine-versions --default-only --engine postgres
+  instance_class         = "db.t3.micro"
+  username               = "postgres"
+  password               = var.postgres_password
+  parameter_group_name   = "default.postgres16"
+  skip_final_snapshot    = true
+  db_subnet_group_name   = aws_db_subnet_group.rds_subnet_group.name
+  vpc_security_group_ids = [aws_security_group.sg_rds.id]
 }
