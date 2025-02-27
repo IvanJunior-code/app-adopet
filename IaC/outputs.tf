de3f84b605c27834ca5fcc75bfd6c53bad06fae2
Ivan Junior <ivan_junior_we@hotmail.com>
2025-02-26 22:34:44 -0300

    Adicionando exibição do endpoint do rds

--- IaC/outputs.tf
+++ IaC/outputs.tf
@@ -1,3 +1,7 @@
 output "connection_ssh_string" {
   value = "ssh -i ${var.path_key}${var.key_name} ubuntu@${aws_instance.ec2_adopet.public_ip}"
 }
+
+output "endpoint_rds" {
+  value = aws_db_instance.rds_postgres.endpoint
+}
