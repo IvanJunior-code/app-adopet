79406f4d1127d4e80c87db0496ab091155a924ac
Ivan Junior <ivan_junior_we@hotmail.com>
2025-02-26 22:33:41 -0300

    Corrigindo script de instalações, adicionando subnet e sg

--- IaC/ec2.tf
+++ IaC/ec2.tf
@@ -1,13 +1,17 @@
 resource "aws_instance" "ec2_adopet" {
-  ami           = "ami-04b4f1a9cf54c11d0"
-  instance_type = "t3.micro"
-  key_name      = var.key_name
+  ami             = "ami-04b4f1a9cf54c11d0"
+  instance_type   = "t3.micro"
+  key_name        = var.key_name
+  subnet_id       = aws_subnet.public_subnet1.id
+  security_groups = [aws_security_group.sg_ssh.id]
 
   user_data = <<-EOF
+                #!/bin/bash
                 sudo apt update && sudo apt-get install nodejs -y && sudo apt install npm -y
-                npm install
-                npm run build
-                npm start:prod
+                sudo apt install postgresql-client-16 -y
+                #npm install
+                #npm run build
+                #npm start:prod
             EOF
 
   tags = {
