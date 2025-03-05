34c21c20a94349eeb0680bd5dffc22b67d5f00b4
Ivan Junior <ivan_junior_we@hotmail.com>
2025-03-05 16:34:54 -0300

    Renomeando grupo de egress, removendo egress duplicado e alterando egress do RDS para o cidr da subnet publica e porta 5432.

--- IaC/network.tf
+++ IaC/network.tf
@@ -75,7 +75,7 @@ resource "aws_vpc_security_group_ingress_rule" "ingress_ssh_rule" {
   to_port           = 22
 }
 
-resource "aws_vpc_security_group_egress_rule" "egress_ssh_rule" {
+resource "aws_vpc_security_group_egress_rule" "egress_rule" {
   security_group_id = aws_security_group.sg_ssh.id
   cidr_ipv4         = "0.0.0.0/0"
   ip_protocol       = "-1"
@@ -85,7 +85,7 @@ resource "aws_security_group" "sg_dns" {
   vpc_id = aws_vpc.vpc.id
 
   tags = {
-    Name = "Security Group DNS"
+    Name = "Security Group Rule"
   }
 }
 
@@ -95,12 +95,6 @@ resource "aws_vpc_security_group_ingress_rule" "ingress_dns_rule" {
   from_port         = 53
   ip_protocol       = "tcp"
   to_port           = 53
-}
-
-resource "aws_vpc_security_group_egress_rule" "egress_dns_rule" {
-  security_group_id = aws_security_group.sg_dns.id
-  cidr_ipv4         = "0.0.0.0/0"
-  ip_protocol       = "-1"
 }
 
 resource "aws_security_group" "sg_rds" {
@@ -121,6 +115,8 @@ resource "aws_vpc_security_group_ingress_rule" "ingress_rds_rule" {
 
 resource "aws_vpc_security_group_egress_rule" "egress_rds_rule" {
   security_group_id = aws_security_group.sg_rds.id
-  cidr_ipv4         = "0.0.0.0/0"
-  ip_protocol       = "-1"
+  cidr_ipv4         = aws_subnet.public_subnet_ec2.cidr_block
+  from_port         = 5432
+  ip_protocol       = "tcp"
+  to_port           = 5432
 }
