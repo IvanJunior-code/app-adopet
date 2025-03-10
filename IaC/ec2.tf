5d18855b9d3d6aafe979eb2c3df18c346d5777a5
Ivan Junior <ivan_junior_we@hotmail.com>
2025-03-10 16:51:17 -0300

    No script, adicionada instalação do jq, inserido path fixo do arquivo dump, adicionando recuperação da senha do banco com uso do cli aws secretsmanager, remoção do arquivo dump sql, e colocada dependencia dos attachments de secretmanager e s3 read na EC2.

--- IaC/ec2.tf
+++ IaC/ec2.tf
@@ -14,7 +14,7 @@ resource "aws_instance" "ec2_adopet" {
                 if [ ! -f /tmp/first_setup_done ]; then
                   {
                     # Instalação do nodejs, npm, postgres-client e unzip
-                    sudo apt-get update && sudo apt-get install nodejs npm postgresql-client-16 unzip -y
+                    sudo apt-get update && sudo apt-get install nodejs npm postgresql-client-16 unzip jq -y
 
                     # Instalação da AWS CLI
                     curl "https://awscli.amazonaws.com/awscli-exe-linux-x86_64.zip" -o "awscliv2.zip"
@@ -22,18 +22,25 @@ resource "aws_instance" "ec2_adopet" {
                     sudo ./aws/install
 
                     # Baixando e configurando o arquivo dump sql
-                    mkdir -p ./sql
-                    aws s3 cp s3://bucket-adopet-dump-sql/adopet-dump ./sql/adopet-dump.sql
-                    chmod 600 ./sql/adopet-dump.sql
-                    chown ubuntu:ubuntu ./sql/adopet-dump.sql
+                    mkdir -p /home/ubuntu/sql/
+                    aws s3 cp s3://bucket-adopet-dump-sql/adopet-dump /home/ubuntu/sql/adopet-dump.sql
+                    chmod 600 /home/ubuntu/sql/adopet-dump.sql
+                    chown ubuntu:ubuntu /home/ubuntu/sql/adopet-dump.sql
+
+                    # Recuperando senha para acesso ao banco de dados
+                    PASSWORD=$(aws secretsmanager get-secret-value --secret-id adopet-db-password --query SecretString --output text | jq -r .password)
 
                     # Restaurando o banco com o arquivo dump .sql
-                    PGPASSWORD="${aws_db_instance.rds_postgres.password}" pg_restore -v -h ${aws_db_instance.rds_postgres.address} -p ${aws_db_instance.rds_postgres.port} -U ${aws_db_instance.rds_postgres.username} -d ${aws_db_instance.rds_postgres.db_name} ./sql/adopet-dump.sql
+                    PGPASSWORD=$PASSWORD pg_restore -v -h ${aws_db_instance.rds_postgres.address} -p ${aws_db_instance.rds_postgres.port} -U ${aws_db_instance.rds_postgres.username} -d ${aws_db_instance.rds_postgres.db_name} /home/ubuntu/sql/adopet-dump.sql
 
                     # Removendo o AWS CLI
                     sudo rm -rf /usr/local/aws-cli
                     sudo rm -rf /usr/bin/aws
                     sudo rm -rf awscliv2.zip aws
+
+                    # Removendo arquivo dump sql
+                    shred -u /home/ubuntu/sql/adopet-dump.sql
+                    sudo rm -rf /home/ubuntu/sql/
 
                     # Criando arquivo de finalização da instalação/configuração
                     touch /tmp/first_setup_done
@@ -45,7 +52,7 @@ resource "aws_instance" "ec2_adopet" {
             EOF
   )
 
-  depends_on = [aws_db_instance.rds_postgres]
+  depends_on = [aws_db_instance.rds_postgres, aws_iam_policy_attachment.iam_secretmanager_policy_attachment, aws_iam_policy_attachment.iam_s3_read_policy_attachment]
 
   tags = {
     Name = "EC2"
