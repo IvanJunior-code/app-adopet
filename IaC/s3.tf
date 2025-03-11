a69117a61dc961cd3eed66655e642322d014ddba
Ivan Junior <ivan_junior_we@hotmail.com>
2025-03-11 17:59:53 -0300

    Criando objeto para hospedar o app compactado

--- IaC/s3.tf
+++ IaC/s3.tf
@@ -1,5 +1,5 @@
-resource "aws_s3_bucket" "bucket" {
-  bucket = "bucket-adopet-dump-sql"
+resource "aws_s3_bucket" "bucket-adopet" {
+  bucket = "bucket-adopet"
 
   tags = {
     Name = "S3 Bucket"
@@ -7,11 +7,23 @@ resource "aws_s3_bucket" "bucket" {
 }
 
 resource "aws_s3_object" "object-dump-sql" {
-  bucket = aws_s3_bucket.bucket.bucket
+  bucket = aws_s3_bucket.bucket-adopet.bucket
   key    = "adopet-dump"
   source = "../sql/adopet-dump.sql"
 
   tags = {
-    Name = "S3 Object"
+    Name = "S3 Object Dump SQL"
+  }
+}
+
+resource "aws_s3_object" "object-app" {
+  bucket = aws_s3_bucket.bucket-adopet.bucket
+  key    = "adopet-app"
+  source = "../app-tar/app.tar"
+
+  depends_on = [null_resource.app_tar]
+
+  tags = {
+    Name = "S3 Object App Tar"
   }
 }
