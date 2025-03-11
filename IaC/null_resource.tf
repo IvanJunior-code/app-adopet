9cb7c1415c18d168d07250999c313e504bbe8a39
Ivan Junior <ivan_junior_we@hotmail.com>
2025-03-11 17:59:09 -0300

    null_resource para criar um arquivo compactado da aplicação.

---
+++ IaC/null_resource.tf
@@ -0,0 +1,13 @@
+resource "null_resource" "app_tar" {
+
+  triggers = {
+    always_run = timestamp()
+  }
+
+  provisioner "local-exec" {
+    command = <<EOT
+        mkdir -p ../app-tar
+        tar -cf ../app-tar/app.tar ../src ../package*.json ../tsconfig*.json
+    EOT
+  }
+}
