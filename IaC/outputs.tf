e0a088043ed48eff0bd8a2a49bc97c5ac4b99b6a
Ivan Junior <ivan_junior_we@hotmail.com>
2025-03-12 14:09:31 -0300

    Output para endereço da aplicação

--- IaC/outputs.tf
+++ IaC/outputs.tf
@@ -5,3 +5,7 @@ output "connection_ssh_string" {
 output "connections_postgres_string" {
   value = "psql -h ${aws_db_instance.rds_postgres.address} -p ${aws_db_instance.rds_postgres.port} -U ${aws_db_instance.rds_postgres.username} -d ${aws_db_instance.rds_postgres.db_name}"
 }
+
+output "teste" {
+  value = "http://${aws_instance.ec2_adopet.public_ip}:${aws_vpc_security_group_ingress_rule.ingress_local_dev_rule.from_port}/adotante"
+}
