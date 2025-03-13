23108bc79c62fa99fee85ec00094410bb7966462
Ivan Junior <ivan_junior_we@hotmail.com>
2025-03-12 22:37:00 -0300

    Preparando o envio do arquivo de inicialização service para a EC2 com aws_ssm_document e associando a EC2 com ssm_association_adopet

---
+++ IaC/ssm.tf
@@ -0,0 +1,34 @@
+resource "aws_ssm_document" "ssm_initialize_adopet" {
+  name          = "adopet-initialize"
+  document_type = "Command"
+  content = jsonencode({
+    schemaVersion = "2.2"
+    description   = "Configurar serviço Adopet"
+    mainSteps = [
+      {
+        action = "aws:runShellScript"
+        name   = "setupAdopet"
+        inputs = {
+          runCommand = [
+            "echo '${file("../service/adopet-backend.service")}' > /etc/systemd/system/adopet-backend.service",
+            "chmod 644 /etc/systemd/system/adopet-backend.service",
+            "systemctl daemon-reload",
+            "systemctl enable adopet-backend.service",
+            "systemctl start adopet-backend.service"
+          ]
+        }
+      }
+    ]
+  })
+
+  depends_on = [aws_instance.ec2_adopet]
+}
+
+resource "aws_ssm_association" "ssm_association_adopet" {
+  name = aws_ssm_document.ssm_initialize_adopet.name
+
+  targets {
+    key    = "InstanceIds"
+    values = [aws_instance.ec2_adopet.id]
+  }
+}
