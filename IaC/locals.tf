8b44636f1905928a39611e39a3539e46b354c8f5
Ivan Junior <ivan_junior_we@hotmail.com>
2025-03-23 21:19:21 -0300

    Adicionada variaveis para subnets publicas do LB

--- IaC/locals.tf
+++ IaC/locals.tf
@@ -1,11 +1,13 @@
 locals {
-  region               = "us-east-1"
-  instance_type        = "t3.micro"
-  ami_id               = "ami-04b4f1a9cf54c11d0"
-  vpc_cidr             = "10.0.0.0/16"
-  subnet_public_cidr   = "10.0.1.0/24"
-  subnet_private1_cidr = "10.0.2.0/24"
-  subnet_private2_cidr = "10.0.3.0/24"
+  region                 = "us-east-1"
+  instance_type          = "t3.micro"
+  ami_id                 = "ami-04b4f1a9cf54c11d0"
+  vpc_cidr               = "10.0.0.0/16"
+  subnet_public_cidr     = "10.0.1.0/24"
+  subnet_private1_cidr   = "10.0.2.0/24"
+  subnet_private2_cidr   = "10.0.3.0/24"
+  subnet_public_lb1_cidr = "10.0.4.0/24"
+  subnet_public_lb2_cidr = "10.0.5.0/24"
 
   management_tags = {
     CreatedBy   = "Terraform"
