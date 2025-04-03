7bbf64d0550616a5a7ff872ef2fe5f2a24b25d1d
Ivan Junior <ivan_junior_we@hotmail.com>
2025-04-03 10:52:52 -0300

    Formatação

--- IaC/ami.tf
+++ IaC/ami.tf
@@ -1,6 +1,7 @@
 resource "aws_ami_from_instance" "adopet_ami" {
-  name               = "adopet_ami"
-  source_instance_id = aws_instance.ec2_adopet.id
+  name                    = "adopet_ami"
+  source_instance_id      = aws_instance.ec2_adopet.id
+  snapshot_without_reboot = false
 
   depends_on = [aws_ssm_association.ssm_association_adopet]
 
