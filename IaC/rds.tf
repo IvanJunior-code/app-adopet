77f5e8b06d525eed138b19a43f2475ba2122ff13
Ivan Junior <ivan_junior_we@hotmail.com>
2025-04-04 17:35:40 -0300

    Alterada a versão de 16 para 17 da engine e do parameter_group_name

--- IaC/rds.tf
+++ IaC/rds.tf
@@ -3,11 +3,11 @@ resource "aws_db_instance" "rds_postgres" {
   db_name                = "mydatabase"
   engine                 = "postgres"
   port                   = 5432
-  engine_version         = "16.5" # aws rds describe-db-engine-versions --default-only --engine postgres
+  engine_version         = "17.2" # aws rds describe-db-engine-versions --default-only --engine postgres
   instance_class         = "db.t3.micro"
   username               = var.postgres_username
   password               = var.postgres_credentials.password
-  parameter_group_name   = "default.postgres16"
+  parameter_group_name   = "default.postgres17"
   skip_final_snapshot    = true
   db_subnet_group_name   = aws_db_subnet_group.rds_subnet_group.name
   vpc_security_group_ids = [aws_security_group.sg_rds.id]
