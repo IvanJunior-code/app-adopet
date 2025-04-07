855fbd4c093fc4098dba800e5331ed74707893c5
Ivan Junior <ivan_junior_we@hotmail.com>
2025-04-07 17:47:56 -0300

    Correção para o ami depender somente o null_resource que valida o app estar pronto.

--- IaC/ami.tf
+++ IaC/ami.tf
@@ -4,8 +4,7 @@ resource "aws_ami_from_instance" "adopet_ami" {
   source_instance_id      = aws_instance.ec2_adopet.id
 
 
-  depends_on = [aws_ssm_association.ssm_association_adopet,
-    aws_ssm_document.ssm_initialize_adopet,
+  depends_on = [
     null_resource.validate_app_ready
   ]
 
