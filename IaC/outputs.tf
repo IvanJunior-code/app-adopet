711ab126f8b5e63e9c3062031df2258c178a495b
Ivan Junior <ivan_junior_we@hotmail.com>
2025-04-08 17:51:07 -0300

    Implementado o comando para testar o app no LB usando o curl.

--- IaC/outputs.tf
+++ IaC/outputs.tf
@@ -6,10 +6,10 @@ output "connections_postgres_string" {
   value = "psql -h ${aws_db_instance.rds_postgres.address} -p ${aws_db_instance.rds_postgres.port} -U ${aws_db_instance.rds_postgres.username} -d ${aws_db_instance.rds_postgres.db_name}"
 }
 
-output "teste" {
+output "ec2_test" {
   value = "http://${aws_instance.ec2_adopet.public_ip}:${aws_vpc_security_group_ingress_rule.ingress_local_dev_rule.from_port}/adotante"
 }
 
-output "lb_dns_name" {
-  value = aws_lb.alb_adopet.dns_name
+output "lb_test" {
+  value = "curl -v http://${aws_lb.alb_adopet.dns_name}/adotante"
 }
